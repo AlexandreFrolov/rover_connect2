33b28a928a222bb264aaf50dd31575be66ff694e
Александр Фролов <frolov@itmatrix.ru>
2024-02-06 13:35:05 +0000

    Fix

--- get-gnss-data.py
+++ get-gnss-data.py
@@ -10,16 +10,14 @@ from rover_connect import SmsTz
 from rover_connect import RoverConnect
 from rover_connect import Telemetry
 
+
 if __name__ == "__main__":
 #    pdb.set_trace()
-
 
     api_url = "http://dbg02-3113.itmatrix.ru:9000/api/data"
     rover = RoverConnect('/dev/ttyS0', 'internet.mts.ru')
     
-    
-    gsm_gnss_data = rover.getGsmLocations()
-    gsm_gnss_info = gsm_gnss_data
+    gsm_gnss_info = rover.getGsmLocations()
         
     # Создаем словарь на основе данных GSMCGNSInfo
     gsm_gnss_info_data = {
@@ -31,8 +29,7 @@ if __name__ == "__main__":
     # Преобразуем словарь в JSON-строку
     gsm_gnss_info_json = json.dumps(gsm_gnss_info_data, ensure_ascii=False, indent=2)  
     gsm_gnss_info_json = json.loads(gsm_gnss_info_json)        
-    
-     
+
     print(gsm_gnss_info_json)
     
 
