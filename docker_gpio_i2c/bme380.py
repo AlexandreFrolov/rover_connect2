17a65424c4c27c15582e8937513187a9539d52d0
Александр Фролов <frolov@itmatrix.ru>
2024-03-29 06:59:24 +0000

    Fix

--- docker_gpio_i2c/bme380.py
+++ docker_gpio_i2c/bme380.py
@@ -4,16 +4,24 @@
 import smbus2
 import bme280
 import json
+import time
 
 port = 1
 address = 0x76
 bus = smbus2.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
-data = bme280.sample(bus, address, calibration_params)
-data_json = {
-"temperature": data.temperature,
-"pressure": data.pressure,
-"humidity": data.humidity
-}
-print(json.dumps(data_json))
+try:
+    print ("Нажмите CTRL+C для завершения")
+    while True:
+      data = bme280.sample(bus, address, calibration_params)
+      data_json = {
+      "temperature": data.temperature,
+      "pressure": data.pressure,
+      "humidity": data.humidity
+      }
+      print(json.dumps(data_json))
+      time.sleep(3)
+except KeyboardInterrupt:
+    print ("Завершено")
+      
