f3f8b4d5dba9448b844640f2d03a6c61ab11e677
Alexandre Frolov <alexandre@frolov.pp.ru>
2024-04-30 07:35:53 +0000

    Fix comments

--- wiring-repka-pi/mblink/mblink.c
+++ wiring-repka-pi/mblink/mblink.c
@@ -4,11 +4,9 @@
 #include <pthread.h>
 #include "wiringRP.h"
 
-// Глобальные переменные и константы
 const int LED_1 = 6;
 const int LED_2 = 7;
 
-// Функции для управления светодиодами в отдельных потоках
 void* led1_blink(void* arg) {
     while (true) {
         digitalWrite(LED_1, HIGH);
@@ -22,43 +20,36 @@ void* led1_blink(void* arg) {
 void* led2_blink(void* arg) {
     while (true) {
         digitalWrite(LED_2, HIGH);
-        delay(300); // Задержка для второго светодиода
+        delay(300);
         digitalWrite(LED_2, LOW);
-        delay(150); // Задержка для второго светодиода
+        delay(150);
     }
     return NULL;
 }
 
 void setup() {
-    // Инициализация библиотек wiringRP
     if (setupWiringRP(WRP_MODE_SUNXI) < 0)
         exit(EXIT_FAILURE);
-    
-    // Инициализация пользовательских объектов
+   
     pinMode(LED_1, OUTPUT);
     pinMode(LED_2, OUTPUT);
     
-    // Создание потоков для управления светодиодами
     pthread_t thread_led1, thread_led2;
     pthread_create(&thread_led1, NULL, led1_blink, NULL);
     pthread_create(&thread_led2, NULL, led2_blink, NULL);
 }
 
 void loop() {
-    // Основной цикл программы
-    // Ничего не делаем здесь, управление светодиодами происходит в отдельных потоках
+	delay(300);
 }
 
 ONDESTROY() {
-    // Освобождение занятых ресурсов, выключение напряжения на пинах
     digitalWrite(LED_1, LOW);
     digitalWrite(LED_2, LOW);
     pinMode(LED_1, DISABLE);
     pinMode(LED_2, DISABLE);
-    
-    // Завершение работы библиотек
     releaseWiringRP();
-    exit(0); // Выход из программы
+    exit(0);
 }
 
 MAIN_WIRINGRP();
