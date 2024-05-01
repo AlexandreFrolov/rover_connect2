512c62b613fe56cc3820cc0c7811d0b730882531
Alexandre Frolov <alexandre@frolov.pp.ru>
2024-05-01 13:55:50 +0000

    Fix Servo

--- wiring-repka-pi/mblink/mtool.c
+++ wiring-repka-pi/mblink/mtool.c
@@ -12,21 +12,25 @@ const int RELAY_1 = 9;
 const int BUTTON_1 = 10;
 bool LED_1_ON = false;
 bool LED_2_ON = false;
-pthread_t thread_led1, thread_led2;
+pthread_t thread_led1, thread_led2, thread_servo1, thread_servo2, thread_servo3;
 
 int pca9685_fd = 0;
 const float HERTZ = 50.0f;
 const int MAX_PWM = 4096;
 const int pca9685Address = 0x40;
-const int pin_x = 15;
-float degree_x = 0.0f;
-int dir_x = 1;
-const int pin_y = 4;
-float degree_y = 50.0f;
-int dir_y = 1;
-const int pin_z = 0;
-float degree_z = 120.0f;
-int dir_z = 0;
+
+const int pin_1 = 0;
+float degree_1 = 0.0f;
+int dir_1 = 0;
+
+const int pin_2 = 4;
+float degree_2 = 0.0f;
+int dir_2 = 0;
+
+const int pin_3 = 8;
+float degree_3 = 0.0f;
+int dir_3 = 0;
+
 
 void pushButton1(__u32 event, __u64 time);
 
@@ -45,8 +49,9 @@ void setNeutralPos() {
     // (View http://en.wikipedia.org/wiki/Servo_control#Pulse_duration)
     float millis = 1.5f;
     int tick = calcTicks(millis, HERTZ);
-    pca9685PwmWrite(pca9685_fd, pin_x, 0, tick);
-    pca9685PwmWrite(pca9685_fd, pin_y, 0, tick);
+	
+//    pca9685PwmWrite(pca9685_fd, pin_x, 0, tick);
+//    pca9685PwmWrite(pca9685_fd, pin_y, 0, tick);
 }
 
 
@@ -75,6 +80,75 @@ void* led2_blink(void* arg) {
     pthread_exit(NULL);
 }
 
+void* thread_servo1_rotate(void* arg) {
+	while (degree_1 < 180) {
+		float millis = mapf(degree_1, 0.0f, 180.0f, 0.5f, 2.5f);
+		printf("degree_1: %.2f, millis: %.2f\n", degree_1, millis);
+		int tick = calcTicks(millis, HERTZ);
+		printf("tick: %d\n", tick);
+		pca9685PwmWrite(pca9685_fd, pin_1, 0, tick);
+		delayMicroseconds(50);
+		
+		degree_1 += dir_1 == 1 ? 0.1f : -0.1f;
+		
+		if (degree_1 > 180) {
+			degree_1 = 180;
+			dir_1 = 0;
+		} else if (degree_1 < 0) {
+			degree_1 = 0;
+			dir_1 = 1;
+		}
+		
+	}
+	degree_1 = 0;
+    pthread_exit(NULL);
+}
+
+void* thread_servo2_rotate(void* arg) {
+	delay(2000);
+	while (degree_2 < 180) {
+		float millis = mapf(degree_2, 0.0f, 180.0f, 0.5f, 2.5f);
+		printf("degree_2: %.2f, millis: %.2f\n", degree_2, millis);
+		int tick = calcTicks(millis, HERTZ);
+		printf("tick: %d\n", tick);
+		pca9685PwmWrite(pca9685_fd, pin_2, 0, tick);
+		delayMicroseconds(50);
+		
+		degree_2 += dir_2 == 1 ? 0.1f : -0.1f;
+		
+		if (degree_2 > 180) {
+			degree_2 = 180;
+			dir_2 = 0;
+		} else if (degree_2 < 0) {
+			degree_2 = 0;
+			dir_2 = 1;
+		}
+		
+	}
+	degree_2 = 0;
+    pthread_exit(NULL);
+}
+
+void* thread_servo3_rotate(void* arg) {
+	int tick;
+	delay(2000);
+	
+	for (int i = 0; i < 3; ++i) {	
+		tick = calcTicks(1.0f, HERTZ);
+		pca9685PwmWrite(pca9685_fd, pin_3, 0, tick);
+		delay(1000);
+		tick = calcTicks(2.0f, HERTZ);
+		pca9685PwmWrite(pca9685_fd, pin_3, 0, tick);
+		delay(1000);
+		tick = calcTicks(1.6f, HERTZ);
+		pca9685PwmWrite(pca9685_fd, pin_3, 0, tick);
+	}
+	
+    pthread_exit(NULL);
+}
+
+
+
 void pushButton1(__u32 event, __u64 time) {
     printf("Button 1 - is %s, timestamp: %lld\n", event == 1 ? "RISING" : "FALLING", time);
     delay(100);
@@ -82,6 +156,9 @@ void pushButton1(__u32 event, __u64 time) {
     if(event && digitalRead(BUTTON_1)) {		
       pthread_create(&thread_led1, NULL, led1_blink, NULL);
 	  pthread_create(&thread_led2, NULL, led2_blink, NULL);
+	  pthread_create(&thread_servo1, NULL, thread_servo1_rotate, NULL);
+	  pthread_create(&thread_servo2, NULL, thread_servo2_rotate, NULL);
+	  pthread_create(&thread_servo3, NULL, thread_servo3_rotate, NULL);
 	}
 }
 
@@ -101,60 +178,9 @@ void setup() {
     delay(1000);	
 }
 
-void moveToX() {
-    float millis = mapf(degree_x, 0.0f, 180.0f, 0.5f, 2.5f);
-    printf("degree_x: %.2f, millis: %.2f\n", degree_x, millis);
-    int tick = calcTicks(millis, HERTZ);
-    printf("tick: %d\n", tick);
-    pca9685PwmWrite(pca9685_fd, pin_x, 0, tick);
-    delayMicroseconds(50);
-    degree_x += dir_x == 1 ? 0.1f : -0.1f;
-    if (degree_x > 180) {
-        degree_x = 180;
-        dir_x = 0;
-    } else if (degree_x < 0) {
-        degree_x = 0;
-        dir_x = 1;
-    }
-}
-void moveToY() {
-    float millis = mapf(degree_y, 0.0f, 180.0f, 0.5f, 2.5f);
-    printf("degree_y: %.2f, millis: %.2f\n", degree_y, millis);
-    int tick = calcTicks(millis, HERTZ);
-    printf("tick: %d\n", tick);
-    pca9685PwmWrite(pca9685_fd, pin_y, 0, tick);
-    delayMicroseconds(50);
-    degree_y += dir_y == 1 ? 0.1f : -0.1f;
-    if (degree_y > 110) {
-        degree_y = 110;
-        dir_y = 0;
-    } else if (degree_y < 60) {
-        degree_y = 60;
-        dir_y = 1;
-    }
-}
-
-void moveToZ() {
-    float millis = mapf(degree_z, 0.0f, 180.0f, 0.5f, 2.5f);
-    printf("degree_z: %.2f, millis: %.2f\n", degree_z, millis);
-    int tick = calcTicks(millis, HERTZ);
-    printf("tick: %d\n", tick);
-    pca9685PwmWrite(pca9685_fd, pin_z, 0, tick);
-    delayMicroseconds(50);
-    degree_z += dir_z == 1 ? 0.1f : -0.1f;
-    if (degree_z > 180) {
-        degree_z = 180;
-        dir_z = 0;
-    } else if (degree_z < 120) {
-        degree_z = 120;
-        dir_z = 1;
-    }
-}
 
 void loop() {
-    moveToX();
-    moveToY();
-    moveToZ();	
+  delay(100);
 }
 
 ONDESTROY() {
